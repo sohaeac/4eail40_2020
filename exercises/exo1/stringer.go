a66fc09f673f73271d3dda565868ee0656e72d08
sohaeac <s.sohaeac@gmail.com>
2020-10-15 21:22:48 +0200

    Exo1 and Exo 2 completed

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -1,19 +1,36 @@
+  
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// Implement types Rectangle, Circle and Shape
+type Shape interface {
+	fmt.Stringer
+}
+
+type Circle struct {
+	radius int
+}
+
+type Rectangle struct {
+	width  int
+	lenght int
+}
+
+func (rec Rectangle) String() string {
+	return fmt.Sprintf("Square of width %d and length %d", rec.width, rec.lenght)
+}
+
+func (cir Circle) String() string {
+	return fmt.Sprintf("Circle of radius %d", cir.radius)
+}
 
 func main() {
 	r := &Rectangle{2, 3}
 	c := &Circle{5}
-
 	shapes := []Shape{r, c}
-
 	for _, s := range shapes {
 		fmt.Println(s)
-		// Expected output:
-		// Square of width 2 and length 3
-		// Circle of radius 5
 	}
 }
--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -10,6 +10,18 @@ type spaceEraser struct {
 	r io.Reader
 }
 
+func (wr spaceEraser) Read(buffer []byte) (int, error) {
+	n, err := wr.r.Read(buffer)
+	numSpaces := 0
+	for i := 0; i < n; i++ {
+		if buffer[i] != 32 {
+			buffer[numSpaces] = buffer[i]
+			numSpaces++
+		}
+	}
+	return numSpaces, err
+}
+
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
 	r := spaceEraser{s}
